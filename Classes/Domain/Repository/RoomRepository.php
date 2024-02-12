8a92dc9f718f6b8403f1c0acb0df5889446ba945
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-12 18:14:18 +0100

    Create RoomRepository.php

---
+++ Classes/Domain/Repository/RoomRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Checkitsedo\VerowaImporter\Domain\Repository;
+
+
+/**
+ * This file is part of the "Verowa Importer" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2021 Dominik Senti <info@senti.lu>, checkit senti.lu
+ */
+
+/**
+ * The repository for Rooms
+ */
+class RoomRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+}
